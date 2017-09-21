2b11034fbde9c253ab8275605c037ff4f17766c8
RadCon4 <a.conradharrison@gmail.com>
2017-09-21 15:55:41 -0500

    Add files via upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -75,7 +75,7 @@ function variableSet() {
 return false;
 });
 
-*/
+
 
 database.ref().on("child_added", function(snapshot) {
 console.log(snapshot.val());
@@ -87,7 +87,9 @@ console.log(snapshot.val());
    // endDate = snapshot.val().endDate;
    searchRadius = snapshot.val().searchRadius;
 
-});
+}); */
+
+
 };;
 // Google Maps API
 
@@ -238,3 +240,86 @@ function eventfulSearch()
     });
 
 }
+
+// CALENDAR API CODE  CURRENTLY ONLY AN EXAMPLE
+
+$(document).ready(function() {
+    
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'listDay,listWeek,month'
+      },
+
+      // customize the button names,
+      // otherwise they'd all just say "list"
+      views: {
+        listDay: { buttonText: 'list day' },
+        listWeek: { buttonText: 'list week' }
+      },
+
+      defaultView: 'listWeek',
+      defaultDate: '2017-09-12',
+      navLinks: true, // can click day/week names to navigate views
+      editable: true,
+      eventLimit: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2017-09-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2017-09-07',
+          end: '2017-09-10'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2017-09-09T16:00:00'
+        },
+        {
+          id: 999,
+          title: 'Repeating Event',
+          start: '2017-09-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2017-09-11',
+          end: '2017-09-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2017-09-12T10:30:00',
+          end: '2017-09-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2017-09-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2017-09-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2017-09-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2017-09-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2017-09-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2017-09-28'
+        }
+      ]
+    });
+    
+  });
